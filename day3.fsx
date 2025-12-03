019db286b3114a80874a3fe6f7032391a5462d8c
Joel Miller <miller345@me.com>
2025-12-03 10:06:22 +0000

    refactor

--- day3.fsx
+++ day3.fsx
@@ -1,21 +1,6 @@
 #load "utils.fsx"
 open Utils
 
-let parseLine (str: string) =
-    str |> Seq.toArray |> Array.map string |> Array.map int
-
-let findMaxJoltage (arr: int array) =
-    let xMax = arr.[0 .. arr.Length - 2] |> Array.max
-    let xPos = Array.findIndex ((=) xMax) arr
-    let yMax = arr.[xPos + 1 ..] |> Array.max
-    (xMax * 10) + yMax
-
-System.IO.File.ReadAllLines("3.txt")
-|> Array.map parseLine
-|> Array.map findMaxJoltage
-|> Array.sum
-|> log
-
 let findNextDigit (arr: int array, targetLength: int) =
     let len = arr.Length
     let reservedTailCount = targetLength - 1 // how many items must i save towards the end of the array
@@ -25,24 +10,26 @@ let findNextDigit (arr: int array, targetLength: int) =
     let tail = arr.[pos + 1 ..] // new array following on from selected max
     max, tail
 
-let findMaxJoltage_p2 (arr: int array, targetLength: int) =
+let findMaxJoltage (arr: int array, targetLength: int) =
     let rec loop (str: string, tail) =
         if str.Length = targetLength then
-            int64 (str)
+            int64 str
         else
-            // let x = log (str, tail)
             let digit, remaining = findNextDigit (tail, targetLength - str.Length)
-
-            if remaining.Length > 0 then
-                loop (str + digit.ToString(), remaining)
-            else
-                int64 (str + digit.ToString())
+            loop (str + digit.ToString(), remaining)
 
     loop ("", arr)
 
 
-System.IO.File.ReadAllLines("3.txt")
-|> Array.map parseLine
-|> Array.map (fun arr -> findMaxJoltage_p2 (arr, 12))
-|> Array.sum
-|> log
+let parseLine (str: string) =
+    str |> Seq.toArray |> Array.map string |> Array.map int
+
+let parsed = System.IO.File.ReadAllLines("3.txt") |> Array.map parseLine
+
+// part 1
+parsed |> Array.map (fun arr -> findMaxJoltage (arr, 2)) |> Array.sum |> log
+// part 2
+parsed |> Array.map (fun arr -> findMaxJoltage (arr, 12)) |> Array.sum |> log
+
+// 17034
+// 168798209663590L
